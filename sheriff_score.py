3121827ccdc4fea7e4a4ec74581b8a6664e67649
Daniel Burton <Daniel.r.burton@gmail.com>
2015-09-18 01:14:15 -0400

    fixed a few variables

--- sheriff_score.py
+++ sheriff_score.py
@@ -6,28 +6,25 @@ get a bonus), announces the king and queen of each good type,
 and then announces the winner. User can then look up individual player
 scores and view score breakdown for that player."""
 
-
-
 class PlayerScore(object):
-    #object for each player's score. Maybe there's an easier way?
     def __init__(self, name):
         self.bread = 0 #initialize all the scores at zero, and the name as name
         self.cheese = 0
         self.chicken = 0
         self.apple = 0
-        self.gold_coins = 0 #actual gold coin tokens
+        self.gold_coin = 0 #actual gold coin tokens
         self.final_score = 0
         self.contraband = 0
         self.name = name.upper()
         self.bonus = 0
 
     def total(self): #recalculate the total score, accounting for different gold value for each good type
-        bread = self.bread * 3 #each of these calculates the gold value of the goods
-        cheese = self.cheese * 3
-        chicken = self.chicken * 4
-        apple = self.apple * 2
+        bread = self.bread * gold_values['bread'] #each of these calculates the gold value of the goods
+        cheese = self.cheese * gold_values['cheese']
+        chicken = self.chicken * gold_values['chicken']
+        apple = self.apple * gold_values['apple']
         self.final_score = bread + self.bonus + cheese \
-        				 + chicken + apple + self.contraband + self.gold_coins
+             + chicken + apple + self.contraband + self.gold_coin
         return self.final_score #make sure this function is not called more than once. Add a way to ensure this?
 
 def reward(players, good):
@@ -42,6 +39,8 @@ def reward(players, good):
 
 players = [] # empty list of all player objects
 
+gold_values = {'apple':2, 'bread':3, 'cheese':3, 'chicken':4,}
+
 while True: #this is kind of ugly?
     next_player = input("Next player name (or none to end): ")
     if next_player.lower() == "none":
@@ -54,9 +53,8 @@ for player in players: #get scores in each category for each player object
     player.cheese = int(input('How many cheese wheels did {} get? '.format(player.name)))
     if len(players) > 3: #bread is only included if there are more than 3 players
         player.bread = int(input('How many loaves of bread did {} get? '.format(player.name)))
-    player.gold_coins = int(input('How many gold coins does {} have? '.format(player.name)))
+    player.gold_coin = int(input('How many gold coins does {} have? '.format(player.name)))
     player.contraband = int(input('Shhh... what is {}\'s contraband score? '.format(player.name)))
-
 
 print('\n\n')
 
@@ -65,21 +63,19 @@ for good in ['apple', 'chicken', 'cheese']:
 
 if len(players) > 3: #rather than these ifs all over for bread, just create a list that stores which goods are being used?
     reward(players, 'bread')
-    
-players.sort(key = lambda x: x.total()) #determine overall winner, simultaneously calling .total() on each
-print('\n\n{} is the winner!'.format(players[-1].name))
+
+players.sort(key = lambda x: x.total(), reverse=True) #determine overall winner, simultaneously calling .total() on each
+print('\n\n{} is the winner!'.format(players[0].name))
 
 while True: #allow lookup of player score breakdown
-	lookup = input("\n\nPlayer whose score breakdown you wish to view: ").upper()
+    lookup = input("\n\nPlayer whose score breakdown you wish to view: ").upper()
 	if lookup not in [score.name for score in players]:
-		print('Try again, that isn\'t a player.\n')
-		continue #returns to beginning of while statement
-	person = next(x for x in players if x.name == lookup) #I already forget what next() does? look into this.
-	print('\nScore for {}.\nGold Pieces from: \n'.format(person.name))
-	if len(players) > 3:
-		print('BREAD: {}.\n'.format(person.bread))
-	for good in ['chicken', 'apple', 'cheese', 'contraband', 'gold_coins', 'bonus']: #see, this is why a list of used goods would help
-		print('{}s: {}.\n'.format(good.upper(), getattr(person, good)))
-	
-	
+        print("Try again, that isn't a player.\n")
+        continue #returns to beginning of while statement
+    person = next(x for x in players if x.name == lookup) #I already forget what next() does? look into this.
+    print('\nScore for {}.\nGold Pieces from: \n'.format(person.name))
+    if len(players) > 3:
+        print('BREAD: {}.\n'.format(person.bread))
+    for good in ['chicken', 'apple', 'cheese', 'contraband', 'gold_coin', 'bonus']: #see, this is why a list of used goods would help
+        print('{}s: {}.\n'.format(good.upper(), gold_values[good] *  getattr(person, good)))
 
