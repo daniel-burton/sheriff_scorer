eb3a62630613f47f50becb1b937806b94e0cb718
daniel-burton <daniel.r.burton@gmail.com>
2018-10-08 18:57:47 -0400

    refactoring and broke final score

--- sheriff_score.py
+++ sheriff_score.py
@@ -1,5 +1,5 @@
 """this program creates a "player score" object for each
-player (from an interactive text prompt) and then gets the 
+player (from an interactive text prompt) and then gets the
 scores in each category for each player. It then calculates the
 bonuses in each category (1st and 2nd place in each "good type"
 get a bonus), announces the king and queen of each good type,
@@ -8,74 +8,96 @@ scores and view score breakdown for that player."""
 
 class PlayerScore(object):
     def __init__(self, name):
-        self.bread = 0 #initialize all the scores at zero, and the name as name
+        # initialize all the scores at zero, name as name
+        self.bread = 0
         self.cheese = 0
         self.chicken = 0
         self.apple = 0
-        self.gold_coin = 0 #actual gold coin tokens
+        self.gold_coin = 0  # actual gold coin tokens
         self.final_score = 0
         self.contraband = 0
-        self.name = name.upper()
+        self.name = name.upper()  # uppercase is more fun
         self.bonus = 0
 
-    def total(self): #recalculate the total score, accounting for different gold value for each good type
-        bread = self.bread * gold_values['bread'] #each of these calculates the gold value of the goods
+    def total(self):
+        """recalculate the total score, accounting for different gold value
+        for each good type"""
+        # each of these calculates gold value of the good
+        bread = self.bread * gold_values['bread']
         cheese = self.cheese * gold_values['cheese']
         chicken = self.chicken * gold_values['chicken']
         apple = self.apple * gold_values['apple']
-        self.final_score = bread + self.bonus + cheese \
-             + chicken + apple + self.contraband + self.gold_coin
-        return self.final_score #make sure this function is not called more than once. Add a way to ensure this?
+        self.final_score = (bread + self.bonus + cheese
+                            + chicken + apple + self.contraband
+                            + self.gold_coin)
+        return self.final_score
 
 def reward(players, good):
-    #give prizes for 1st and 2nd place for each good
-    #todo: add actual values of prizes for 1st and 2nd place, they are not the same in the real game.
-    prizes = {'chicken': (20, 10), 'apple': (10, 5), 'bread': (10, 5), 'cheese': (10, 5)} #dictionary stores value for each type
+    # give prizes for 1st and 2nd place for each good
+    prizes = {'chicken': (20, 10), 'apple': (10, 5),
+              'bread': (10, 5), 'cheese': (10, 5),
+              'contraband': (0, 0), 'gold': (0, 0)}
     king, queen = prizes[good]
-    players = sorted(players, key = lambda x: getattr(x, good, 0))
-    players[-1].bonus += king #top player is king, 2nd place is queen
+    players = sorted(players, key=lambda x: getattr(x, good, 0))
+    players[-1].bonus += king  # top player is king, 2nd place is queen
     players[-2].bonus += queen
-    print("{} is the king of {}, {} is the queen.".format(players[-1].name, good, players[-2].name))
+    print("{} is the {} king, {} is the queen.".format(players[-1].name,
+                                                       good,
+                                                       players[-2].name))
 
-players = [] # empty list of all player objects
+def ask_score(player, good):
+    """create the string to ask the score for each good to eliminate
+    duplicate code"""
+    ask = {"apple": "many apples", "bread": "many loaves of bread",
+           "cheese": "many wheels of cheese", "chicken": "many chickens",
+           "contraband": "much contraband", "gold": "many gold coins"}
+    score = int(input("How {} did {} get? ".format(ask[good], player.name)))
+    return score
 
-gold_values = {'apple':2,'bread':3,'cheese':3,'chicken':4,'contraband':1,'bonus':1, 'gold_coin':1}
+players = []  # empty list of all player objects
 
-while True: #this is kind of ugly?
-    next_player = input("Next player name (or none to end): ")
-    if next_player.lower() == "none":
-        break #keep asking for player names until get "none"
-    players.append(PlayerScore(next_player)) #add initialized score object to list of players, with next_player as player.name
+gold_values = {'apple': 2, 'bread': 3, 'cheese': 3, 'chicken': 4,
+               'contraband': 1, 'bonus': 1, 'gold_coin': 1}
 
-for player in players: #get scores in each category for each player object
-    player.apple = int(input('\n\nHow many apples did {} get? ' .format(player.name)))
-    player.chicken = int(input('How many chickens did {} get? '.format(player.name)))
-    player.cheese = int(input('How many cheese wheels did {} get? '.format(player.name)))
-    if len(players) > 3: #bread is only included if there are more than 3 players
-        player.bread = int(input('How many loaves of bread did {} get? '.format(player.name)))
-    player.gold_coin = int(input('How many gold coins does {} have? '.format(player.name)))
-    player.contraband = int(input('Shhh... what is {}\'s contraband score? '.format(player.name)))
+if __name__ == "__main__":
+    while True:
+        next_player = input("Next player name (or none to end): ")
+        if next_player.lower() == "none" or next_player.lower() == "":
+            break  # keep asking for player names until get "none"
+        players.append(PlayerScore(next_player))
+        # add initialized score object to list of players
 
-print('\n\n')
-
-for good in ['apple', 'chicken', 'cheese']:
-    reward(players, good)
-
-if len(players) > 3: #rather than these ifs all over for bread, just create a list that stores which goods are being used?
-    reward(players, 'bread')
-
-players.sort(key = lambda x: x.total(), reverse=True) #determine overall winner, simultaneously calling .total() on each
-print('\n\n{} is the winner!'.format(players[0].name))
-
-while True: #allow lookup of player score breakdown
-    lookup = input("\n\nPlayer whose score breakdown you wish to view: ").upper()
-    if lookup not in [score.name for score in players]:
-        print("Try again, that isn't a player.\n")
-        continue #returns to beginning of while statement
-    person = next(x for x in players if x.name == lookup) #I already forget what next() does? look into this.
-    print('\nScore for {}.\nGold Pieces from: \n'.format(person.name))
+    goods = ['apple', 'cheese', 'chicken', 'contraband', 'gold']
     if len(players) > 3:
-        print('BREAD: {}.\n'.format(person.bread))
-    for good in ['chicken', 'apple', 'cheese', 'contraband', 'gold_coin', 'bonus']: #see, this is why a list of used goods would help
-        print('{}s: {}.\n'.format(good.upper(), gold_values[good] *  getattr(person, good)))
+        goods.append('bread')
 
+    for player in players:  # get score in each category for each player object
+        for item in goods:
+            player.item = ask_score(player, item)
+
+    print('\n\n')
+
+    for item in goods:
+        reward(players, item)
+
+    players.sort(key=lambda x: x.total(), reverse=True)
+    # determine overall winner, simultaneously calling .total() on each
+    print('\n\n{} is the winner!'.format(players[0].name))
+
+    while True:  # allow lookup of player score breakdown
+        lookup = input(("\n\nPlayer whose score breakdown you wish to view"
+                        " (or hit enter to end program): """)).upper()
+        if lookup == "":
+            print("Thanks for playing!\n")
+            break
+        if lookup not in [score.name for score in players]:
+            print("Try again, that isn't a player.\n")
+            continue  # returns to beginning of while statement
+        person = next(x for x in players if x.name == lookup)
+        print('\nScore for {}.\nGold Pieces from: \n'.format(person.name))
+        if len(players) > 3:
+            print('BREAD: {}.\n'.format(person.bread * gold_values['bread']))
+        for item in ['chicken', 'apple', 'cheese', 'contraband', 'gold_coin',
+                     'bonus']:
+            print('{}: {}.\n'.format(item.upper(),
+                                     gold_values[item] * getattr(person, item)))
